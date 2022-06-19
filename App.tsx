fef2b3fc78c24b1a43acb2f0db6ce02cb6073e82
vitorcontiz <vitorcontizgit@gmail.com>
2022-06-19 12:29:39 -0300

    final features

--- App.tsx
+++ App.tsx
@@ -3,8 +3,12 @@ import { FlatList, SafeAreaView, ScrollView, StatusBar, StyleSheet, View } from 
 import { Card } from './components/Card'
 import { IFilms } from './interfaces/films'
 import { getFilms } from './services/loadData'
-
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat'
 export default function App() {
+	const [fonteCarregada] = useFonts({
+		MontserratRegular: Montserrat_400Regular,
+		MontserratBold: Montserrat_700Bold,
+	})
 	const [films, setFilms] = useState<IFilms[]>([])
 	useEffect(() => {
 		;(async function () {
--- components/ModalDetailFilm.tsx
+++ components/ModalDetailFilm.tsx
@@ -10,21 +10,37 @@ const ModalDetailFilm: React.FC<any> = ({ isVisible, closeModal, filmId }: { isV
 			const responseFilmDetail = await getFilmById(filmId)
 			setFilmDetail(responseFilmDetail)
 		})()
-	}, [])
-
+	}, [filmId])
+	function getStarRate(rate: string | undefined) {
+		const parse = parseInt(rate!)
+		const ratingArray = ['★★', '★★★', '★★★★', '★★★★★']
+		if (parse <= 15) {
+			return ratingArray[0]
+		} else if (parse > 15 && parse < 40) {
+			return ratingArray[1]
+		} else if (parse > 40 && parse <= 94) {
+			return ratingArray[2]
+		} else {
+			return ratingArray[3]
+		}
+	}
 	return (
 		<Modal visible={isVisible} animationType='fade'>
 			<View style={styles.modalConteiner}>
 				<Pressable style={styles.backButton} onPress={closeModal}>
 					<Text style={styles.textButton}>{'<'}</Text>
 				</Pressable>
+				<Text style={styles.filmTitle}>{filmDetail?.title}</Text>
 				<Image
 					style={styles.bannerFilm}
 					source={{
 						uri: filmDetail?.movie_banner,
 					}}
 				/>
-				<Text>{filmDetail?.description}</Text>
+				<View style={styles.detailsContainer}>
+					<Text style={styles.filmDescription}>{filmDetail?.description}</Text>
+					<Text style={styles.rate}>{getStarRate(filmDetail?.rt_score)}</Text>
+				</View>
 			</View>
 		</Modal>
 	)
@@ -34,20 +50,42 @@ export default ModalDetailFilm
 
 const styles = StyleSheet.create({
 	modalConteiner: {
+		flex: 1,
 		alignItems: 'center',
-		justifyContent: 'center',
+		backgroundColor: '#01243a',
+	},
+	detailsContainer: {
+		alignItems: 'center',
 	},
 	backButton: {
-		backgroundColor: '#4E3064',
+		backgroundColor: '#012e4a',
 		alignItems: 'center',
 		width: '100%',
 	},
 	textButton: {
-		fontSize: 40,
+		fontSize: 60,
 		color: '#fff',
 	},
 	bannerFilm: {
-		width: 100,
+		width: '100%',
 		height: 300,
+	},
+	filmTitle: {
+		marginVertical: 8,
+		fontSize: 24,
+		fontWeight: '700',
+		color: '#e2d3ed',
+		textAlign: 'center',
+	},
+	filmDescription: {
+		textAlign: 'justify',
+		fontSize: 18,
+		fontFamily: 'MontserratRegular',
+		marginHorizontal: 8,
+		color: '#cccccc',
+	},
+	rate: {
+		fontSize: 40,
+		color: '#dec507',
 	},
 })
